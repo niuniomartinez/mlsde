1b61cd91e5fcdac4e0c904b6f78be651a0f4b5d0
Guillermo Martínez Jiménez <25800862+niuniomartinez@users.noreply.github.com>
2021-05-12 14:45:02 +0200

    Fixed makedocs.sh script, as it didn't parse gui nor configuration directories.

--- makedocs.sh
+++ makedocs.sh
@@ -2,8 +2,10 @@
 
 # This script builds the internal source documentation.
 SOURCES=src/*.pas
+SRCCFG=src/configurationdialog/*.pas
+SRCGUI=src/gui/*.pas
 SRCSYNT=src/syntax/*.pas
 OUTPUT_DIR=docs/internal/src/
 EXT_HIERARCHY=--external-class-hierarchy=pasdoc-inheritance.txt
 
-pasdoc -T mlsde -O html -L en --use-tipue-search --include-creation-time -E "$OUTPUT_DIR" $EXT_HIERARCHY "$SOURCES" "$SRCSYNT"
+pasdoc -T mlsde -O html -L en --use-tipue-search --include-creation-time -E "$OUTPUT_DIR" $EXT_HIERARCHY "$SOURCES" "$SRCCFG" "$SRCGUI" "$SRCSYNT"
